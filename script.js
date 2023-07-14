98f5d5aab0c331fa67b998e9ac71b7a80d536955
Amit Kumar Das <102554819+amitdas1880@users.noreply.github.com>
2023-07-14 20:24:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const key =document.getElementsByTagName("p")[0];
-const p =document.getElementsByTagName("p")[1];
+const p =document.getElementsByTagName("strong")[1];
 key.addEventListener("click", highlight)
 function highlight() {
     p.style.color="green";
