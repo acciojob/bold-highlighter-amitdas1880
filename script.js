11a3a58aae7e9caa2ad653a2c2e9f5ac51db42d6
Amit Kumar Das <102554819+amitdas1880@users.noreply.github.com>
2023-07-16 11:04:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-let boldWords = document.querySelectorAll(' p strong');
+
 
 function highlight() {
-//      let boldWords = document.querySelectorAll('p strong');
+      let boldWords = document.querySelectorAll('p strong');
     for(let i = 0; i < boldWords.length; i++) {
         boldWords[i].style.color = 'green';
     }
@@ -10,6 +10,7 @@ function highlight() {
 
 
 function return_normal() {
+	let boldWords = document.querySelectorAll('p strong');
     for(let i = 0; i < boldWords.length; i++) {
         boldWords[i].style.color = 'black';
 }
